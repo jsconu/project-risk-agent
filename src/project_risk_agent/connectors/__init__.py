d24608ae3d665f6bb827e8a12f523853b149abb5
jsconu <80986551+jsconu@users.noreply.github.com>
2026-09-15 15:48:02 -0600

    feat: export Jira connector

--- src/project_risk_agent/connectors/__init__.py
+++ src/project_risk_agent/connectors/__init__.py
@@ -2,5 +2,6 @@
 
 from project_risk_agent.connectors.base import ProjectConnector
 from project_risk_agent.connectors.file import FileConnector
+from project_risk_agent.connectors.jira import JiraConnector
 
-__all__ = ["FileConnector", "ProjectConnector"]
+__all__ = ["FileConnector", "JiraConnector", "ProjectConnector"]
