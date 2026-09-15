931295cd23afc65c2dcd5cc70ca6ca50e096fbce
jsconu <80986551+jsconu@users.noreply.github.com>
2026-09-15 15:35:38 -0600

    Add connector package exports

---
+++ src/project_risk_agent/connectors/__init__.py
@@ -0,0 +1,6 @@
+"""Project data connectors."""
+
+from project_risk_agent.connectors.base import ProjectConnector
+from project_risk_agent.connectors.file import FileConnector
+
+__all__ = ["ProjectConnector", "FileConnector"]
